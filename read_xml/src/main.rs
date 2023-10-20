3c0517881ab4203c1d5c5f093871e0e4bcef79a8
palutz <stefano13@duck.com>
2023-10-20 17:20:56 +0100

    simple xml read with xml-rs

---
+++ read_xml/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("file.xml")?;
+    let file = BufReader::new(file); // Buffering is important for performance
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{:spaces$}+{name}", "", spaces = depth * 2);
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{:spaces$}-{name}", "", spaces = depth * 2);
+            }
+            Err(e) => {
+                eprintln!("Error: {e}");
+                break;
+            }
+            // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
