e5dfdcc57cef84e44891d898cecfb294ca5329dc
palutz <stefano13@duck.com>
2024-01-17 20:08:14 +0000

    diff messages

--- sled-actors/src/main.rs
+++ sled-actors/src/main.rs
@@ -1,4 +1,5 @@
 use tokio::sync::{oneshot, mpsc};
+use tokio::time::{sleep, Duration };
 
 struct MyActor {
     receiver: mpsc::Receiver<ActorMessage>,
@@ -8,6 +9,7 @@ enum ActorMessage {
     GetUniqueId {
         respond_to: oneshot::Sender<u32>,
     },
+    OtherMessage,
 }
 
 impl MyActor {
@@ -21,6 +23,7 @@ impl MyActor {
         match msg {
             ActorMessage::GetUniqueId { respond_to } => {
                 self.next_id += 1;
+                dbg!("Received msg {:?}", self.next_id);
 
                 // The `let _ =` ignores any errors when sending.
                 //
@@ -28,15 +31,26 @@ impl MyActor {
                 // to cancel waiting for the response.
                 let _ = respond_to.send(self.next_id);
             },
+            _ => { let _ = sleep(Duration::from_millis(500)); }
+        }
+    }
+
+    async fn run(&mut self) {
+        // if all the receivers are dropped then recv() will return None and this loop
+        // will stop, the function will return and the actor shutdown
+        while let Some(msg) = self.receiver.recv().await {
+            self.handle_message(msg);
         }
     }
 }
 
-async fn run_my_actor(mut actor: MyActor) {
-    while let Some(msg) = actor.receiver.recv().await {
-        actor.handle_message(msg);
-    }
-}
+// async fn run_my_actor(mut actor: MyActor) {
+//     // if all the receivers are dropped then recv() will return None and this loop
+//     // will stop, the function will return and the actor shutdown
+//     while let Some(msg) = actor.receiver.recv().await {
+//         actor.handle_message(msg);
+//     }
+// }
 
 
 #[derive(Clone)]
@@ -47,8 +61,8 @@ pub struct MyActorHandle {
 impl MyActorHandle {
     pub fn new() -> Self {
         let (sender, receiver) = mpsc::channel(8);
-        let actor = MyActor::new(receiver);
-        tokio::spawn(run_my_actor(actor));
+        let mut actor = MyActor::new(receiver);
+        tokio::spawn(async move { actor.run().await });
 
         Self { sender }
     }
@@ -65,14 +79,24 @@ impl MyActorHandle {
         let _ = self.sender.send(msg).await;
         recv.await.expect("Actor task has been killed")
     }
+
+    pub async fn other_msg(&self) {
+        let (_, recv) = oneshot::channel();
+        let msg = ActorMessage::OtherMessage;
+
+        let _ = self.sender.send(msg).await;
+        recv.await.expect("Actor task has been killed")
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let myh = MyActorHandle::new();
 
-    let id = myh.get_unique_id().await;
-
-    println!("Actor id = {id}");
+    for i  in 10..21 {
+        let id = myh.get_unique_id();
+        println!("Req {i} sent to actor");
+        //println!("{i} Actor id = {}", id.await);
+    }
 
 }
